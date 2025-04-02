e5a1527d8f655438639ce2d087b39831dea9fa3e
Les04k <mybrazzerzz@gmail.com>
2025-04-02 09:01:52 +0300

    База данных

---
+++ saloon.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 02 2025 г., 09:00
+-- Версия сервера: 5.7.39
+-- Версия PHP: 8.0.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `saloon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `personal`
+--
+
+CREATE TABLE `personal` (
+  `id_personal` int(255) NOT NULL,
+  `loginss` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `passs` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fio` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` int(50) NOT NULL,
+  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `personal`
+--
+
+INSERT INTO `personal` (`id_personal`, `loginss`, `passs`, `fio`, `age`, `role`) VALUES
+(1, 'manager', 'manager', 'Иванов Аркадий Сргеевич', 25, 'Менеджер');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(255) NOT NULL,
+  `fio` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logins` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `passs` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `uslugi`
+--
+
+CREATE TABLE `uslugi` (
+  `id_uslug` int(255) NOT NULL,
+  `name_uslug` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price_uslug` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `zapiss`
+--
+
+CREATE TABLE `zapiss` (
+  `id_zapisi` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_personal` int(11) NOT NULL,
+  `id_uslugi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `personal`
+--
+ALTER TABLE `personal`
+  ADD PRIMARY KEY (`id_personal`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Индексы таблицы `uslugi`
+--
+ALTER TABLE `uslugi`
+  ADD PRIMARY KEY (`id_uslug`);
+
+--
+-- Индексы таблицы `zapiss`
+--
+ALTER TABLE `zapiss`
+  ADD PRIMARY KEY (`id_zapisi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_uslugi` (`id_uslugi`),
+  ADD KEY `id_personal` (`id_personal`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `personal`
+--
+ALTER TABLE `personal`
+  MODIFY `id_personal` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `uslugi`
+--
+ALTER TABLE `uslugi`
+  MODIFY `id_uslug` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `zapiss`
+--
+ALTER TABLE `zapiss`
+  MODIFY `id_zapisi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `zapiss`
+--
+ALTER TABLE `zapiss`
+  ADD CONSTRAINT `zapiss_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
+  ADD CONSTRAINT `zapiss_ibfk_3` FOREIGN KEY (`id_uslugi`) REFERENCES `uslugi` (`id_uslug`),
+  ADD CONSTRAINT `zapiss_ibfk_4` FOREIGN KEY (`id_personal`) REFERENCES `personal` (`id_personal`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
